86afb553687796947de20452facdfa1f7282280d
KarimKashkoush <karimkashkoush5@gmail.com>
2024-01-02 22:10:44 +0200

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ let navBar = document.getElementById("navBar");
 
 window.onscroll = ()=> {
     if(scrollY > 50) {
-        navBar.style.boxShadow = 'rgba(0, 0, 0, 0.35) 0px 5px 15px'
+        navBar.style.boxShadow = 'rgba(0, 0, 0, 0.1) 0px 5px 15px'
     } else {
         navBar.style.boxShadow = "none"
     }
